3dca328ebc21ce2ae99b8613fffc05517c46ad70
ethanol190 <cloudygumption@gmail.com>
2020-06-03 16:39:51 +1000

    added component chem-list

---
+++ ChemicalApp/src/app/chem-list/chem-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chem-list',
+  templateUrl: './chem-list.component.html',
+  styleUrls: ['./chem-list.component.scss']
+})
+export class ChemListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
