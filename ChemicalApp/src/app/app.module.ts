ec876dd9e04c08f11f2a8c782aa111289d75e266
ethanol190 <cloudygumption@gmail.com>
2020-06-03 18:13:18 +1000

    removed unrequired module

--- ChemicalApp/src/app/app.module.ts
+++ ChemicalApp/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChemListComponent } from './chem-list/chem-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormsModule,
-    ChemListComponent
+    FormsModule
   ],
   imports: [
     BrowserModule,
